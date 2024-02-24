ef5c98e016247c07c8cc0c11b2233bea0e1b6edb
Sintel <sintel.inquiries@gmail.com>
2024-02-24 19:05:53 +0100

    fix multiline error reports

--- src/span.rs
+++ src/span.rs
@@ -36,15 +36,16 @@ impl Span {
 }
 
 // https://github.com/pest-parser/pest/blob/1e407663b4aeef32e481643d0c45d834799af404/pest/src/position.rs
+// both start at zero! add one for displaying
 fn line_col(input: &str, pos: usize) -> (usize, usize) {
 	let slice = &input[..pos];
 
 	let prec_ln = memchr::memrchr(b'\n', slice.as_bytes());
 	if let Some(prec_nl_pos) = prec_ln {
-		let lines = bytecount::count(slice[..=prec_nl_pos].as_bytes(), b'\n') + 1;
-		(lines, slice[prec_nl_pos..].chars().count())
+		let lines = bytecount::count(slice[..=prec_nl_pos].as_bytes(), b'\n');
+		(lines, slice[prec_nl_pos..].chars().count() - 1)
 	} else {
-		(1, slice.chars().count() + 1)
+		(0, slice.chars().count())
 	}
 }
 
@@ -61,38 +62,44 @@ pub fn format_note(message: &str, span: Span, input: &str) {
 }
 
 pub fn print_message(message: &str, span: Span, input: &str, level: &'static str) {
-	// TODO: this only works properly if the span is one line
-
 	let (startl, startc, endl, endc) = span.line_col(input);
 
-	let mut linepos = startl;
-
-	let mut spaces = String::new();
-	for _ in linepos.to_string().chars() {
-		spaces.push(' ');
-	}
+	let width = (endl + 1).to_string().chars().count();
+	let spaces = " ".repeat(width);
 
 	// TODO fix the filename
-	eprintln!("{level}: src\\file:{startl}: {message}");
+	eprintln!("{level}: blua\\test.blua:{}: {message}", startl + 1);
 	eprintln!("{spaces} |");
 
-	for l in input.lines().skip(startl - 1) {
+	for (lc, l) in input.lines().skip(startl).enumerate() {
+		if l.is_empty() {
+			continue;
+		}
+		let mut start = startc;
+		let mut end = endc;
+		if lc > 0 {
+			start = 0
+		}
+		if lc < end {
+			end = l.len()
+		}
+
 		let mut underline = String::new();
-		for c in l.chars().take(startc - 1) {
+		for c in l.chars().take(start) {
 			match c {
 				'\t' => underline.push('\t'),
 				_ => underline.push(' '),
 			}
 		}
-
-		for _ in 0..(endc - startc) {
+		for _ in 0..(end - start) {
 			underline.push('^');
 		}
 
-		eprintln!("{linepos} | {l}");
+		let linepos = lc + startl + 1;
+		eprintln!("{linepos:width$} | {l}", width = width);
 		eprintln!("{spaces} | {underline}");
-		linepos += 1;
-		if linepos >= endl {
+		// linepos += 1;
+		if lc >= endl - startl {
 			break;
 		}
 	}
