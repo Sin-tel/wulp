89d038f0c20948b002abce94e4bc82ab0272792b
Sintel <sintel.inquiries@gmail.com>
2024-03-18 21:39:33 +0100

    make index a macro

--- src/index.rs
+++ src/index.rs
@@ -1,90 +1,44 @@
-// This should be a macro but I just really love copy-pasting things
 use crate::span::InputFile;
 use crate::symbol::Symbol;
 use crate::ty::TyNode;
 use std::fmt;
 use std::ops::{Index, IndexMut};
 
-#[derive(Debug, Copy, Clone, Ord, PartialOrd, Eq, PartialEq, Hash)]
-pub struct TyId(pub u32);
+macro_rules! id_type {
+	($id:ident, $value:ident) => {
+		#[derive(Debug, Copy, Clone, Ord, PartialOrd, Eq, PartialEq, Hash)]
+		pub struct $id(pub u32);
 
-impl fmt::Display for TyId {
-	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-		write!(f, "{}", self.0)
-	}
+		impl fmt::Display for $id {
+			fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+				write!(f, "{}", self.0)
+			}
+		}
+		impl Index<$id> for [$value] {
+			type Output = $value;
+			fn index(&self, index: $id) -> &$value {
+				&self[index.0 as usize]
+			}
+		}
+		impl Index<$id> for Vec<$value> {
+			type Output = $value;
+			fn index(&self, index: $id) -> &$value {
+				&self.as_slice()[index]
+			}
+		}
+		impl IndexMut<$id> for [$value] {
+			fn index_mut(&mut self, index: $id) -> &mut $value {
+				&mut self[index.0 as usize]
+			}
+		}
+		impl IndexMut<$id> for Vec<$value> {
+			fn index_mut(&mut self, index: $id) -> &mut $value {
+				&mut self.as_mut_slice()[index]
+			}
+		}
+	};
 }
 
-impl Index<TyId> for [TyNode] {
-	type Output = TyNode;
-
-	fn index(&self, index: TyId) -> &TyNode {
-		&self[index.0 as usize]
-	}
-}
-
-impl IndexMut<TyId> for [TyNode] {
-	fn index_mut(&mut self, index: TyId) -> &mut TyNode {
-		&mut self[index.0 as usize]
-	}
-}
-
-impl Index<TyId> for Vec<TyNode> {
-	type Output = TyNode;
-	fn index(&self, index: TyId) -> &TyNode {
-		&self.as_slice()[index]
-	}
-}
-
-impl IndexMut<TyId> for Vec<TyNode> {
-	fn index_mut(&mut self, index: TyId) -> &mut TyNode {
-		&mut self.as_mut_slice()[index]
-	}
-}
-
-#[derive(Debug, Copy, Clone, Ord, PartialOrd, Eq, PartialEq, Hash)]
-pub struct SymbolId(pub u32);
-
-impl fmt::Display for SymbolId {
-	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-		write!(f, "{}", self.0)
-	}
-}
-
-impl Index<SymbolId> for [Symbol] {
-	type Output = Symbol;
-
-	fn index(&self, index: SymbolId) -> &Symbol {
-		&self[index.0 as usize]
-	}
-}
-
-impl Index<SymbolId> for Vec<Symbol> {
-	type Output = Symbol;
-	fn index(&self, index: SymbolId) -> &Symbol {
-		&self.as_slice()[index]
-	}
-}
-
-#[derive(Debug, Copy, Clone, Ord, PartialOrd, Eq, PartialEq, Hash)]
-pub struct FileId(pub u32);
-
-impl fmt::Display for FileId {
-	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-		write!(f, "{}", self.0)
-	}
-}
-
-impl Index<FileId> for [InputFile] {
-	type Output = InputFile;
-
-	fn index(&self, index: FileId) -> &InputFile {
-		&self[index.0 as usize]
-	}
-}
-
-impl Index<FileId> for Vec<InputFile> {
-	type Output = InputFile;
-	fn index(&self, index: FileId) -> &InputFile {
-		&self.as_slice()[index]
-	}
-}
+id_type!(TyId, TyNode);
+id_type!(SymbolId, Symbol);
+id_type!(FileId, InputFile);
